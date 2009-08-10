6ddc144c55901d88a043d7a9ccd8ef35e823dbfd
tamacat.org <tamacat.org@gmail.com>
2009-08-10 15:51:13 +0000

    Share project "tamacat-httpd-samples" into "https://tamacat.googlecode.com/svn/trunk"

---
+++ tamacat-httpd-samples/src/main/java/org/tamacat/httpd/samples/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2009, TamaCat.org
+ * All rights reserved.
+ */
+package org.tamacat.httpd.samples;
+
+import org.tamacat.httpd.Httpd;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Httpd.main(args);
+	}
+}
