1aba86ae9c6514776ced314dcb1a2d1deba66786
tamacat.org@gmail.com <tamacat.org@gmail.com@9b12007c-730a-11de-9528-39508ac20fff>
2009-08-10 15:51:13 +0000

    Share project "tamacat-httpd-samples" into "https://tamacat.googlecode.com/svn/trunk"

---
+++ trunk/tamacat-httpd-samples/src/main/java/org/tamacat/httpd/samples/AccessControlFilter.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2009, TamaCat.org
+ * All rights reserved.
+ */
+package org.tamacat.httpd.samples;
+
+import org.tamacat.httpd.filter.AbstractAccessControlFilter;
+
+public class AccessControlFilter extends AbstractAccessControlFilter {
+
+	@Override
+	protected boolean isSuccess(String url) {
+		return true;
+	}
+}
