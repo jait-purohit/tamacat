920d60451a4830517eb1e53d5656b73f77f30a05
tamacat.org@gmail.com <tamacat.org@gmail.com@9b12007c-730a-11de-9528-39508ac20fff>
2009-08-25 13:33:09 +0000

    v0.4 release.

---
+++ tags/tamacat-httpd-samples-0.4/bin/startup.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set JAVA_OPT="-server"
+
+if not "%JAVA_HOME%" == "" goto OkJHome
+echo.
+echo ERROR: JAVA_HOME not found in your environment.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation
+echo.
+goto error
+
+:OkJHome
+SET JAVA_EXE="%JAVA_HOME%\bin\java.exe"
+
+if "%OS%"=="Windows_NT" SET SERVER_HOME=%~dp0\..
+
+set CLASSPATH_JAR=%SERVER_HOME%\;%SERVER_HOME%\conf
+
+for %%i in ("%SERVER_HOME%"\lib\*.jar) do call classpath_jar.bat %%i
+rem echo %CLASSPATH_JAR%
+
+%JAVA_EXE% %JAVA_OPT% -classpath %CLASSPATH_JAR% -Dserver.home=%SERVER_HOME% org.tamacat.httpd.Httpd
+
