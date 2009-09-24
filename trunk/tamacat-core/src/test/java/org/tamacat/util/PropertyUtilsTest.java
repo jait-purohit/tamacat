318d724c6d5853d454f3765ab49436439ddb8f45
tamacat.org@gmail.com <tamacat.org@gmail.com@9b12007c-730a-11de-9528-39508ac20fff>
2009-09-24 14:17:53 +0000

    Bug fixed (xml loading)

--- trunk/tamacat-core/src/test/java/org/tamacat/util/PropertyUtilsTest.java
+++ trunk/tamacat-core/src/test/java/org/tamacat/util/PropertyUtilsTest.java
@@ -14,7 +14,7 @@ public class PropertyUtilsTest {
 
     static final String path1 = "org/tamacat/di/DIContainer.properties";
     static final String path2 = "org.tamacat.di.DIContainer.properties";
-    static final String path3 = "org.tamacat.di.DIContainer";
+    static final String path3 = "test.xml";
 
     static final String path4 = "org/tamacat/di/not_found.properties";
 
@@ -33,6 +33,26 @@ public class PropertyUtilsTest {
 //                props3.getProperty("DIContainerClassLoader"));
 //    }
 
+    @Test
+    public void testGetProperties() {
+        try {
+            Properties props1 = PropertyUtils.getProperties(path1);
+            assertNotNull(props1);
+            
+            Properties props2 = PropertyUtils.getProperties(path2);
+            assertNotNull(props2);
+
+            Properties props3 = PropertyUtils.getProperties(path3);
+            assertNotNull(props3);
+            
+            Properties props4 = PropertyUtils.getProperties("log4j.properties");
+            assertNotNull(props4);
+        } catch (Exception e) {
+        	e.printStackTrace();
+        	fail();
+        }
+    }
+    
     @Test
     public void testGetPropertiesNotFound() {
         try {
