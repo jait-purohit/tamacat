dbfbf04be14d5c711acf977ac570409ccd89d11c
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2009-11-16 15:30:15 +0000

    add test case.

--- trunk/tamacat-core/src/main/java/org/tamacat/util/FileUtils.java
+++ trunk/tamacat-core/src/main/java/org/tamacat/util/FileUtils.java
@@ -8,14 +8,19 @@ public class FileUtils {
     static final char SEPARATOR = File.separator.charAt(0);
 
     public static String getFilePath(String name) {
-    	return name !=  null ? name.replace(SEPARATOR, '/') : "";
+    	return name != null ? name.replace(SEPARATOR, '/') : "";
     }
     
     public static URL getRelativePathToURL(String name) {
+    	String dir = System.getProperty("user.dir");
+    	return getRelativePathToURL(dir, name);
+    }
+    
+    public static URL getRelativePathToURL(String root, String name) {
+    	String dir = root != null ? root : "";
     	try {
     	    String file = getFilePath(name);
     	    if (file.length() > 0 && file.charAt(0) != '/') {
-    	    	String dir = System.getProperty("user.dir");
     			dir = dir != null ? dir.replace(SEPARATOR, '/') + '/' : "/";
     			if (dir.charAt(0) != '/') dir = "/" + dir;
     			file = dir + file;
