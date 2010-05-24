28085e8aaf7301920980159ad58e8608e9991d6b
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2010-05-24 16:03:15 +0000

    update v0.7 add sample HTML5+HDFS upload

---
+++ tamacat-httpd-samples/src/main/java/org/tamacat/httpd/samples/action/TopAction.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2010, TamaCat.org
+ * All rights reserved.
+ */
+package org.tamacat.httpd.samples.action;
+
+import org.tamacat.httpd.core.RequestContext;
+
+public class TopAction {
+
+	public void top(RequestContext request) {
+		System.out.println("Top/top()");
+	}
+}
