9bb8d603c2cd5dfce3c5ea5fa0ad8f176427b5cc
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-03-10 20:45:09 +0000

    bugfix -> public class

--- trunk/tamacat-httpd/src/main/java/org/tamacat/httpd/session/DefaultSession.java
+++ trunk/tamacat-httpd/src/main/java/org/tamacat/httpd/session/DefaultSession.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 import org.tamacat.util.UniqueCodeGenerator;
 
-class DefaultSession implements Session, Serializable {
+public class DefaultSession implements Session, Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Date creationDate;
