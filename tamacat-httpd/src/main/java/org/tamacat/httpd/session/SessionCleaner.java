6a753a2b99c5da2b161a45d9cec01bb87b9b4590
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-04-20 15:13:38 +0000

    refacturing session cleaner.

--- tamacat-httpd/src/main/java/org/tamacat/httpd/session/SessionCleaner.java
+++ tamacat-httpd/src/main/java/org/tamacat/httpd/session/SessionCleaner.java
@@ -10,11 +10,20 @@ public class SessionCleaner implements Runnable {
 	static final Log LOG = LogFactory.getLog(SessionCleaner.class);
 
 	private int checkInterval =  30 * 1000; //default 30sec.
+	private String name = "Cleaner";
 	
 	private SessionFactory manager;
 	
-	public SessionCleaner(SessionFactory manager) {
+	public void setSessionFactory(SessionFactory manager) {
 		this.manager = manager;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
 	}
 	
 	public void setCheckInterval(int checkInterval) {
