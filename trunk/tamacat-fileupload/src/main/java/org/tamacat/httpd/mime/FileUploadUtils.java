d71283dc9b24050cb339b042280979689a2952a4
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-05-14 15:02:01 +0000

    add MessageDigest support

---
+++ trunk/tamacat-fileupload/src/main/java/org/tamacat/httpd/mime/FileUploadUtils.java
@@ -0,0 +1,19 @@
+package org.tamacat.httpd.mime;
+
+import java.util.List;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.http.protocol.HttpContext;
+import org.tamacat.httpd.filter.HttpFilter;
+
+public class FileUploadUtils {
+
+	@SuppressWarnings("unchecked")
+	public static List<HttpFileItem> getFileItemList(HttpContext context) {
+		return (List<HttpFileItem>) context.getAttribute(FileItem.class.getName());
+	}
+	
+	public static Exception getException(HttpContext context) {
+		return (Exception) context.getAttribute(HttpFilter.EXCEPTION_KEY);
+	}
+}
