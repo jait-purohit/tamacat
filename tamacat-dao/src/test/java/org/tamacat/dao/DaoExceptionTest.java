b0b3981d9488a4efc9a8ad3b3450648d1878290c
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-07-27 14:14:01 +0000

    add testcase

---
+++ tamacat-dao/src/test/java/org/tamacat/dao/DaoExceptionTest.java
@@ -0,0 +1,32 @@
+package org.tamacat.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DaoExceptionTest {
+
+	@Test
+	public void testDaoExceptionString() {
+		DaoException e = new DaoException("TEST ERROR");
+		assertEquals("TEST ERROR", e.getMessage());
+		assertEquals(null, e.getCause());
+	}
+
+	@Test
+	public void testDaoExceptionThrowable() {
+		Exception cause = new RuntimeException("TEST ERROR");
+		DaoException e = new DaoException(cause);
+		assertEquals("java.lang.RuntimeException: TEST ERROR", e.getMessage());
+		assertSame(cause, e.getCause());
+	}
+
+	@Test
+	public void testDaoExceptionStringThrowable() {
+		Exception cause = new RuntimeException("CAUSE ERROR");
+		DaoException e = new DaoException("TEST ERROR", cause);
+		assertEquals("TEST ERROR", e.getMessage());
+		assertEquals("CAUSE ERROR", e.getCause().getMessage());
+	}
+
+}
