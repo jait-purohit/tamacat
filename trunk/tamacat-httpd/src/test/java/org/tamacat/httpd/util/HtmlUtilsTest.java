db7b680eedcbc9a7b212a1744e084ce7b966fa6f
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-10-19 19:46:17 +0000

    add test and comments.

--- trunk/tamacat-httpd/src/test/java/org/tamacat/httpd/util/HtmlUtilsTest.java
+++ trunk/tamacat-httpd/src/test/java/org/tamacat/httpd/util/HtmlUtilsTest.java
@@ -10,6 +10,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.http.Header;
+import org.apache.http.message.BasicHeader;
 import org.junit.Before;
 import org.junit.Test;
 import org.tamacat.httpd.util.HtmlUtils;
@@ -115,11 +117,50 @@ public class HtmlUtilsTest {
 	
 	@Test
 	public void testGetCharset() {
-		String html1 = "<html><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=utf-8\"></html>";
+		Header header = new BasicHeader("Content-Type", "text/html; charset=UTF-8");
+		assertEquals("utf-8", HtmlUtils.getCharSet(header));
+	}
 	
+	@Test
+	public void testGetCharsetDefault() {
+		Header header = new BasicHeader("Content-Type", "text/html");
+		assertEquals(null, HtmlUtils.getCharSet(header));
+	}
+	
+	@Test
+	public void testGetCharSetFromMetaTag0() {
+		String html1 = "<html><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=utf-8\"></html>";
 		Matcher matcher = pattern.matcher(html1);
 		if (matcher.find()) {
 			assertEquals("utf-8", matcher.group(4));
 		}
+	}
+	
+	@Test
+	public void testGetCharSetFromMetaTag() {
+		String html = "<html><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\"></html>";
+		String result = HtmlUtils.getCharSetFromMetaTag(html, "");
+		assertEquals("utf-8", result);
+	}
+	
+	@Test
+	public void testGetCharSetFromMetaTag2() {
+		String html = "<html><META HTTP-EQUIV='Content-Type' CONTENT='text/html; charset=UTF-8'></html>";
+		String result = HtmlUtils.getCharSetFromMetaTag(html, "");
+		assertEquals("utf-8", result);
+	}
+	
+	@Test
+	public void testGetCharSetFromMetaTag3() {
+		String html = "<html><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html;charset=UTF-8\"></html>";
+		String result = HtmlUtils.getCharSetFromMetaTag(html, "");
+		assertEquals("utf-8", result);
+	}
+	
+	@Test
+	public void testGetCharSetFromMetaTagDefault() {
+		String html = "<html><META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html\"></html>";
+		String result = HtmlUtils.getCharSetFromMetaTag(html, "utf-8");
+		assertEquals("utf-8", result);
 	}
 }
