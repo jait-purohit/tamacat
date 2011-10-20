0291f3bf946835a9f8c7634fc4ad3d82bda73709
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-10-20 20:04:28 +0000

    add test.

--- tamacat-httpd/src/test/java/org/tamacat/httpd/html/LinkConvertingEntityTest.java
+++ tamacat-httpd/src/test/java/org/tamacat/httpd/html/LinkConvertingEntityTest.java
@@ -33,6 +33,66 @@ public class LinkConvertingEntityTest {
 		
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		entity.writeTo(out);
+		assertEquals(html.getContentLength(), entity.getContentLength());
 		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+	}
+	
+	@Test
+	public void testWriteToOutputStream2() throws Exception {
+		StringEntity html = new StringEntity("<html><a href=\"/aaaaa/test.html\">aaa</a></html>");
+		String before = "/aaaaa/";
+		String after = "/bbb/";
+		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
+		assertNotNull(entity);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		entity.writeTo(out);
+		assertEquals(html.getContentLength()-2, entity.getContentLength());
+		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+	}
+	
+	@Test
+	public void testWriteToOutputStream3() throws Exception {
+		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">aaa</a></html>");
+		String before = "/aaa/";
+		String after = "/bbbbb/";
+		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
+		assertNotNull(entity);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		entity.writeTo(out);
+		assertEquals(html.getContentLength()+2, entity.getContentLength());
+		assertEquals("<html><a href=\"/bbbbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+	}
+	
+	
+	@Test
+	public void testWriteToOutputStream_NAME() throws Exception {
+		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">/aaa/</a></html>");
+		String before = "/aaa/";
+		String after = "/bbbbb/";
+		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
+		assertNotNull(entity);
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		entity.writeTo(out);
+		assertEquals(html.getContentLength()+2, entity.getContentLength());
+		assertEquals("<html><a href=\"/bbbbb/test.html\">/aaa/</a></html>", new String(out.toByteArray()));
+	}
+	
+	@Test
+	public void testWriteToOutputStream_ERROR() throws Exception {
+		StringEntity html = new StringEntity("<html><a href=\"/test.html\">/aaaaa/</a></html>");
+		String before = "/aaaaa/";
+		String after = "/bbbbb/";
+		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
+		assertNotNull(entity);
+		
+		try {
+			entity.writeTo(null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Output stream may not be null", e.getMessage());
+		}
 	}
 }
