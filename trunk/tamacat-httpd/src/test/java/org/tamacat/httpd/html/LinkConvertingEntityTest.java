d00fc144dc3ceb86263194adc0bd7821a98ba99f
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2011-10-24 19:20:52 +0000

    for IPv6 bugfix test error.

--- trunk/tamacat-httpd/src/test/java/org/tamacat/httpd/html/LinkConvertingEntityTest.java
+++ trunk/tamacat-httpd/src/test/java/org/tamacat/httpd/html/LinkConvertingEntityTest.java
@@ -25,7 +25,7 @@ public class LinkConvertingEntityTest {
 
 	@Test
 	public void testWriteToOutputStream() throws Exception {
-		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">aaa</a></html>");
+		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">aaa</a></html>\r\n");
 		String before = "/aaa/";
 		String after = "/bbb/";
 		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
@@ -36,12 +36,12 @@ public class LinkConvertingEntityTest {
 		System.out.println(new String(out.toByteArray()));
 
 		assertEquals(html.getContentLength(), entity.getContentLength());
-		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>\r\n", new String(out.toByteArray()));
 	}
 	
 	@Test
 	public void testWriteToOutputStream2() throws Exception {
-		StringEntity html = new StringEntity("<html><a href=\"/aaaaa/test.html\">aaa</a></html>");
+		StringEntity html = new StringEntity("<html><a href=\"/aaaaa/test.html\">aaa</a></html>\r\n");
 		String before = "/aaaaa/";
 		String after = "/bbb/";
 		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
@@ -50,12 +50,12 @@ public class LinkConvertingEntityTest {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		entity.writeTo(out);
 		assertEquals(html.getContentLength()-2, entity.getContentLength());
-		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+		assertEquals("<html><a href=\"/bbb/test.html\">aaa</a></html>\r\n", new String(out.toByteArray()));
 	}
 	
 	@Test
 	public void testWriteToOutputStream3() throws Exception {
-		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">aaa</a></html>");
+		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">aaa</a></html>\r\n");
 		String before = "/aaa/";
 		String after = "/bbbbb/";
 		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
@@ -64,13 +64,13 @@ public class LinkConvertingEntityTest {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		entity.writeTo(out);
 		assertEquals(html.getContentLength()+2, entity.getContentLength());
-		assertEquals("<html><a href=\"/bbbbb/test.html\">aaa</a></html>", new String(out.toByteArray()));
+		assertEquals("<html><a href=\"/bbbbb/test.html\">aaa</a></html>\r\n", new String(out.toByteArray()));
 	}
 	
 	
 	@Test
 	public void testWriteToOutputStream_NAME() throws Exception {
-		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">/aaa/</a></html>");
+		StringEntity html = new StringEntity("<html><a href=\"/aaa/test.html\">/aaa/</a></html>\r\n");
 		String before = "/aaa/";
 		String after = "/bbbbb/";
 		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
@@ -79,12 +79,12 @@ public class LinkConvertingEntityTest {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		entity.writeTo(out);
 		assertEquals(html.getContentLength()+2, entity.getContentLength());
-		assertEquals("<html><a href=\"/bbbbb/test.html\">/aaa/</a></html>", new String(out.toByteArray()));
+		assertEquals("<html><a href=\"/bbbbb/test.html\">/aaa/</a></html>\r\n", new String(out.toByteArray()));
 	}
 	
 	@Test
 	public void testWriteToOutputStream_ERROR() throws Exception {
-		StringEntity html = new StringEntity("<html><a href=\"/test.html\">/aaaaa/</a></html>");
+		StringEntity html = new StringEntity("<html><a href=\"/test.html\">/aaaaa/</a></html>\r\n");
 		String before = "/aaaaa/";
 		String after = "/bbbbb/";
 		LinkConvertingEntity entity = new LinkConvertingEntity(html, before, after);
