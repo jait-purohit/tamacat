6faf6a19b706edb113cd5366555048e5ee4d788f
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2012-06-14 13:33:09 +0000

    1.0 release

---
+++ tags/tamacat-core-1.0/src/test/java/org/tamacat/util/DateUtilsTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2007, tamacat.org
+ * All rights reserved.
+ */
+package org.tamacat.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testGetDateTime() {
+        System.out.println(DateUtils.getTimestamp("yyyy-MM-dd HH:mm:ss,S"));
+        assertTrue(true);
+    }
+
+    @Test
+    public void testParseTime() {
+        System.out.println(DateUtils.parse("2011-01-01 00:00:00", "yyyy-MM-dd HH:mm:ss"));
+        assertTrue(true);
+    }
+
+}
