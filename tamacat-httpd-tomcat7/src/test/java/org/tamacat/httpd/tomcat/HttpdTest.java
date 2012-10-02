97fbfc0182ddefb4806d9b0afe3ed5915404a72a
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2012-10-02 13:43:34 +0000

    tamacat-httpd Tomcat7 integration. (tamacat-httpd-tomcat7)

---
+++ tamacat-httpd-tomcat7/src/test/java/org/tamacat/httpd/tomcat/HttpdTest.java
@@ -0,0 +1,10 @@
+package org.tamacat.httpd.tomcat;
+
+import org.tamacat.httpd.Httpd;
+
+public class HttpdTest {
+	
+	public static void main(String[] args) throws Exception {
+		Httpd.main(args);
+	}
+}
