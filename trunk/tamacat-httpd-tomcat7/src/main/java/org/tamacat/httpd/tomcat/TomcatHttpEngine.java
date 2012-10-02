f968dcfe131a24cf830d4a4b03674ab85efc4a16
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2012-10-02 13:43:34 +0000

    tamacat-httpd Tomcat7 integration. (tamacat-httpd-tomcat7)

---
+++ trunk/tamacat-httpd-tomcat7/src/main/java/org/tamacat/httpd/tomcat/TomcatHttpEngine.java
@@ -0,0 +1,22 @@
+package org.tamacat.httpd.tomcat;
+
+import org.tamacat.httpd.core.HttpEngine;
+
+/**
+ * <p>It is implements of the multi-thread server.
+ * The embedded Tomcat is supported. 
+ */
+public class TomcatHttpEngine extends HttpEngine {
+
+	@Override
+	public void init() {
+		super.init();
+		TomcatManager.start();
+	}
+	
+	@Override
+	public void stopHttpd() {
+		TomcatManager.stop();
+		super.stopHttpd();
+	}
+}
