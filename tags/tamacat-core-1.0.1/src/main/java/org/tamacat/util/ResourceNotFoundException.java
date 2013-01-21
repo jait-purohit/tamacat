368a1f8b48920584bda19f3635e6b82c71e76703
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2013-01-21 13:42:39 +0000

    release version 1.0.1

---
+++ tags/tamacat-core-1.0.1/src/main/java/org/tamacat/util/ResourceNotFoundException.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2007, tamacat.org
+ * All rights reserved.
+ */
+package org.tamacat.util;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
