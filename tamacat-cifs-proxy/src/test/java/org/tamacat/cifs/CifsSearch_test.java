4e650d973368c483b1556f8ace768be0c34d2eb1
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2013-09-24 21:50:26 +0000

    update  libs

--- tamacat-cifs-proxy/src/test/java/org/tamacat/cifs/CifsSearch_test.java
+++ tamacat-cifs-proxy/src/test/java/org/tamacat/cifs/CifsSearch_test.java
@@ -3,6 +3,7 @@ package org.tamacat.cifs;
 import java.io.File;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
@@ -14,13 +15,14 @@ import org.apache.lucene.store.FSDirectory;
 public class CifsSearch_test {
 	
 	public static void main(String[] args) throws Exception {
-	    IndexReader reader = IndexReader.open(FSDirectory.open(new File("./src/test/resources/index/")));
+	    IndexReader reader = DirectoryReader.open(FSDirectory.open(new File("./src/test/resources/index/")));
 	    IndexSearcher searcher = new IndexSearcher(reader);
 
 	    Query query = new TermQuery(new Term("name", "pdf"));
-	    TopDocs rs = searcher.search(query, null, 100);
+	    int max=100;
+	    TopDocs rs = searcher.search(query, null, max);
 	    System.out.println(rs.totalHits);
-	    for (int i=0; i<rs.totalHits; i++) {
+	    for (int i=0; i<rs.scoreDocs.length; i++) {
 	    	Document firstHit = searcher.doc(rs.scoreDocs[i].doc);
 	    	System.out.println(firstHit.get("folder")+firstHit.get("name"));
 	    }
