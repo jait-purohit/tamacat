28a9f604bf664cf64059c6153a7b9ac41b255f2e
tamacat.org <tamacat.org@9b12007c-730a-11de-9528-39508ac20fff>
2014-02-01 00:36:58 +0000

    update junit and test case.

--- tamacat-httpd/src/test/java/org/tamacat/httpd/auth/DynamicRealmTest.java
+++ tamacat-httpd/src/test/java/org/tamacat/httpd/auth/DynamicRealmTest.java
@@ -6,13 +6,13 @@ package org.tamacat.httpd.auth;
 
 import java.util.Date;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.junit.Test;
 import org.tamacat.util.DateUtils;
 
 public class DynamicRealmTest {
-	
+
 	@Test
 	public void testRealmDate() {
 		Date date = new Date();
